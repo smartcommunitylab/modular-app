f7b3a095c2208f9a6fdab2f49e0ddc71c29c72e0
chin8 <matteo.chini@fbk.eu>
2019-06-20 09:54:51 +0200

    cleaned timetable

--- modular-app/src/app/module-trasporti/lazy.routing.ts
+++ modular-app/src/app/module-trasporti/lazy.routing.ts
@@ -7,11 +7,6 @@ const routes: Routes = [
     { path: 'ttlist/:ref', loadChildren: './pages/transport-list/transport-list.module#TransportListPageModule' },
     { path: 'ttgroup/:ref/:agencyId/:groupId', loadChildren: './pages/transport-list/transport-list.module#TransportListPageModule' },
     { path: 'tt/:ref/:agencyId/:groupId/:routeId/:title', loadChildren: './pages/transport-tt/transport-tt.module#TransportTtPageModule' },
-    { path: 'plan', loadChildren: './pages/plan/plan.module#PlanPageModule' },
-    { path: 'parking/:ref', loadChildren: './pages/parking/parking.module#ParkingPageModule' },
-    { path: 'parkingMeters', loadChildren: './pages/parking-meters/parking-meters.module#ParkingMetersPageModule' },
-    { path: 'bikesharing/:ref', loadChildren: './pages/bikesharing/bikesharing.module#BikesharingPageModule' },
-    { path: 'taxi', loadChildren: './pages/taxi/taxi.module#TaxiPageModule' },
     { path: 'transport-tt', loadChildren: './pages/transport-tt/transport-tt.module#TransportTtPageModule' },
     { path: 'map-spots', loadChildren: './pages/map-spots/map-spots.module#MapSpotsPageModule' }
 ];
