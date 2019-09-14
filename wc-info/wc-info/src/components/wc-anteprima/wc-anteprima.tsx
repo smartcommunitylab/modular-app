53c83facb83ef97c291197b5c0d16b341dc82831
chin8 <matteo.chini@fbk.eu>
2019-09-14 15:46:46 +0200

    new home setting, new module info

--- wc-info/wc-info/src/components/wc-anteprima/wc-anteprima.tsx
+++ wc-info/wc-info/src/components/wc-anteprima/wc-anteprima.tsx
@@ -198,11 +198,34 @@ export class cwAnteprima {
           ? <img src={this.img}></img>
           : ""
         }
+         
+        {/* <div class="margine"></div>
         <div class="descrizione">
           <div id="centra">
           </div>
-        </div>
+        </div> */}
         <div id="Informazioni">
+        <div class="margine"></div>
+         <div class="contacts">
+          {this.link && this.link != ""
+            ?          <div class="external-container">
+            <div class="contact-container" onClick={() => this.openLink(this.link)} > 
+            <div class="icon icon-contact">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z" fill="#11b3ef"/></svg>
+            </div>  
+          </div>
+        </div>
+            : ""
+          }
+          {this.web && this.link != ""
+            ? <div class="external-container"><div class="contact-container" onClick={() => this.openLink(this.web)} >
+                        <div class="icon icon-contact">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M74.6 256c0-38.3 31.1-69.4 69.4-69.4h88V144h-88c-61.8 0-112 50.2-112 112s50.2 112 112 112h88v-42.6h-88c-38.3 0-69.4-31.1-69.4-69.4zm85.4 22h192v-44H160v44zm208-134h-88v42.6h88c38.3 0 69.4 31.1 69.4 69.4s-31.1 69.4-69.4 69.4h-88V368h88c61.8 0 112-50.2 112-112s-50.2-112-112-112z" fill="#11b3ef"/></svg>
+            </div>  </div></div>
+
+            : ""
+          }
+          </div>
           <div id="data" innerHTML={this.datapubblicazione}></div>
           <div id="abstract" innerHTML={this.abstract}></div>
           <div class="testo">
@@ -211,17 +234,7 @@ export class cwAnteprima {
 
 
 
-          <div class="margine"></div>
-          {this.link && this.link != ""
-            ? <div class="external-link" onClick={() => this.openLink(this.link)} >DOWNLOAD</div>
 
-            : ""
-          }
-          {this.web && this.link != ""
-            ? <div class="external-link" onClick={() => this.openLink(this.web)} >APRI</div>
-
-            : ""
-          }
         </div>
       </div>
     ];
